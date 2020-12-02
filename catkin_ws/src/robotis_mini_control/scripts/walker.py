fe89266cf01d238917d104483aa39f4f56a05e00
Dhruv Mathur <dhruv.mathur3@gmail.com>
2020-12-01 22:56:29 -0600

    lunge pose added

--- catkin_ws/src/robotis_mini_control/scripts/walker.py
+++ catkin_ws/src/robotis_mini_control/scripts/walker.py
@@ -22,12 +22,12 @@ class SinusoidFunction:
         self.amplitude=1
         self.phase_offset=0
         self.angular_frequency=1
-        
+
     def get(self, x):
         """ x between 0 and 1"""
         f = math.sin(self.phase_offset + self.angular_frequency*x)
-        return self.amplitude_offset + self.amplitude*f        
-        
+        return self.amplitude_offset + self.amplitude*f
+
     def clone(self):
         z=SinusoidFunction()
         z.amplitude_offset=self.amplitude_offset
@@ -35,35 +35,35 @@ class SinusoidFunction:
         z.phase_offset=self.phase_offset
         z.angular_frequency=self.angular_frequency
         return z
-    
+
     def mirror(self):
         z=self.clone()
         z.amplitude_offset *= -1
         z.amplitude *= -1
         return z
-    
+
     def mirror_keep_amplitude_offset(self):
         z=self.clone()
         z.amplitude *= -1
         return z
-        
+
     def mirror_freq(self):
         z=self.clone()
         z.phase_offset *= -1
         z.angular_frequency *= -1
         return z
-        
+
     def __str__(self):
         return "y=%.2f+%.2f*sin(%.2f+%.2f*x)"%(self.amplitude_offset, self.amplitude, self.phase_offset, self.angular_frequency)
-        
+
 class WholeBodyWalkerFunction:
     """
-    Multi-joint walk function for RobotisMini  
+    Multi-joint walk function for RobotisMini
     Creates SinusoidFunction for each joint with different parameters
     """
     def __init__(self, walking_params):
         self.parameters={}
-        
+
         print(walking_params)
         for pn, pp in walking_params.items():
             self.parameters[pn + '_amplitude']=pp[0]
@@ -71,19 +71,19 @@ class WholeBodyWalkerFunction:
             self.parameters[pn + '_phase_offset']=pp[2]
 
         self.parameters["step_frequency"]=math.pi
-        
+
         self.parameters["vx_amplitude"]=0.5
         self.parameters["vy_amplitude"]=0.5
         self.parameters["vt_amplitude"]=0.4
-        
+
         self.generate()
-        
+
     def generate(self):
         """
         Build CPG functions for walk-on-spot (no translation or rotation, only legs up/down)
-        """        
-        
-        self.pfn={} # phase joint functions    
+        """
+
+        self.pfn={} # phase joint functions
         self.afn={} # anti phase joint functions
 
         ## Foot and hip -> Lateral motion
@@ -92,10 +92,10 @@ class WholeBodyWalkerFunction:
         foot_func.amplitude= self.parameters["foot_amplitude"]
         foot_func.amplitude_offset= self.parameters["foot_amplitude_offset"]
         foot_func.phase_offset= self.parameters["foot_phase_offset"]
-        self.pfn["l_foot_joint"]=foot_func        
+        self.pfn["l_foot_joint"]=foot_func
         foot_func_af=foot_func.mirror()
         self.afn["l_foot_joint"]=foot_func_af
-        
+
         hip_func=SinusoidFunction()
         hip_func.angular_frequency= self.parameters["step_frequency"]
         hip_func.amplitude= self.parameters["hip_amplitude"]
@@ -104,7 +104,7 @@ class WholeBodyWalkerFunction:
         self.pfn["l_hip_joint"]=hip_func
         hip_func_af=hip_func.mirror()
         self.afn["l_hip_joint"]=hip_func_af
-        
+
         ## Thigh, ankle and knee -> Frontal motion
         thigh_func=SinusoidFunction()
         thigh_func.angular_frequency= self.parameters["step_frequency"]
@@ -114,7 +114,7 @@ class WholeBodyWalkerFunction:
         self.pfn["l_thigh_joint"]=thigh_func
         thigh_func_af=thigh_func.mirror_keep_amplitude_offset()
         self.afn["l_thigh_joint"]=thigh_func_af
-        
+
         ankle_func=SinusoidFunction()
         ankle_func.angular_frequency= self.parameters["step_frequency"]
         ankle_func.amplitude= self.parameters["ankle_amplitude"]
@@ -123,7 +123,7 @@ class WholeBodyWalkerFunction:
         self.pfn["l_ankle_joint"]=ankle_func
         ankle_func_af=ankle_func.mirror_keep_amplitude_offset()
         self.afn["l_ankle_joint"]=ankle_func_af
-        
+
         knee_func=SinusoidFunction()
         knee_func.angular_frequency= self.parameters["step_frequency"]
         knee_func.amplitude= self.parameters["knee_amplitude"]
@@ -132,7 +132,7 @@ class WholeBodyWalkerFunction:
         self.pfn["l_knee_joint"]=knee_func
         knee_func_af=knee_func.mirror_keep_amplitude_offset()
         self.afn["l_knee_joint"]=knee_func_af
-        
+
         #f3=SinusoidFunction()
         #f3.angular_frequency=self.parameters["step_frequency"]
         #f3.amplitude=self.parameters["step_amplitude"]
@@ -145,20 +145,20 @@ class WholeBodyWalkerFunction:
         #f4=f3.mirror()
         ##f4.amplitude_offset -= 0.4
         #self.pfn["l_knee_joint"]=f4
-        
+
         #f5=f3.mirror_keep_amplitude_offset()
         #self.afn["l_thigh_joint"]=f5
-        
+
         #f6=f33.mirror_keep_amplitude_offset()
         #self.afn["l_ankle_joint"]=f6
         #f7=f5.mirror()
         ##f7.amplitude_offset -= 0.4
         #self.afn["l_knee_joint"]=f7
-        
+
         self.generate_right()
-        
+
         self.show()
-              
+
     def generate_right(self):
         """
         Mirror CPG functions from left to right and antiphase right
@@ -169,7 +169,7 @@ class WholeBodyWalkerFunction:
             self.pfn["r_"+j].phase_offset += math.pi
             self.afn["r_"+j]=self.pfn["l_"+j].mirror_keep_amplitude_offset()
             self.afn["r_"+j].phase_offset += math.pi
-        
+
     def get(self, phase, x, velocity):
         """ Obtain the joint angles for a given phase, position in cycle (x 0,1)) and velocity parameters """
         angles={}
@@ -181,18 +181,18 @@ class WholeBodyWalkerFunction:
                 angles[j]=self.afn[j].get(x)
         self.apply_velocity(angles,velocity,phase,x)
         return angles
-                   
+
     def show(self):
         """
         Display the CPG functions used
         """
         for j in self.pfn.keys():
-            print(j,"p",self.pfn[j],"a",self.afn[j])        
+            print(j,"p",self.pfn[j],"a",self.afn[j])
         print(self.pfn["l_knee_joint"].amplitude_offset)
 
     def apply_velocity(self, angles, velocity, phase, x):
         """ Modify on the walk-on-spot joint angles to apply the velocity vector"""
-        
+
         # VX
         v=velocity[0]*self.parameters["vx_amplitude"]
         d=(x*2-1)*v
@@ -233,7 +233,7 @@ class WholeBodyWalkerFunction:
                 angles["l_foot_joint"]+=d
                 angles["r_hip_joint"]-=d
                 angles["r_foot_joint"]-=d
-                
+
         ## VT
         #v=velocity[2]*self.parameters["vt_amplitude"]
         #d=(x)*v
@@ -258,15 +258,15 @@ class Walker:
     Class for making RobotisMini walk
     """
     def __init__(self, robotis_mini_ci, real_robot):
-        
+
         self.robotis_mini_ci=robotis_mini_ci
-        
+
         self.real_robot = real_robot
-        
+
         self.displacing=False #When the robot is walking AND displacing in the plane (velocity is non-zero)
         self.walking=False  #When the robot is walking: moving up and down the legs
         self.velocity=[0,0,0]
-        
+
         #Default walking params
         self.walking_params = {}
         self.walking_params['foot'] = [0.4,0,0]
@@ -274,7 +274,7 @@ class Walker:
         self.walking_params['knee'] = [0.4,0.7,0]
         self.walking_params['thigh'] = [-0.4,-0.7,0]
         self.walking_params['hip'] = [0.4,0,0]
-        
+
         self.wb_walkerfunc=WholeBodyWalkerFunction(self.walking_params)
 
         self.initial_wq = self.wb_walkerfunc.get(True, 0, [0,0,0])  #First joint configuration to start the walking motion
@@ -283,16 +283,16 @@ class Walker:
         j_names=self.initial_wq.keys()
         for jn in j_names:
             print(jn + str(":") + str(self.initial_wq[jn]))
-        
+
         self._th_walk=None  #Walking thread
-        
+
         self._cycle_period = 5 #seconds
 
         self._sub_cmd_vel=rospy.Subscriber(robotis_mini_ci.ns+"cmd_vel", Twist,self._cb_cmd_vel, queue_size=1)
         self._sub_cmd_stop=rospy.Subscriber(robotis_mini_ci.ns+"stop_srv", Empty,self._cb_cmd_stop, queue_size=1)
         self._sub_cmd_restart=rospy.Subscriber(robotis_mini_ci.ns+"restart_srv", Empty,self._cb_cmd_restart, queue_size=1)
         self._sub_cmd_restart=rospy.Subscriber(robotis_mini_ci.ns+"walking_params", Float64MultiArray,self._cb_new_walking_params, queue_size=1)
-        
+
         if not self.real_robot:
             self.pubs = {}
             # Wait until the joints have been populated
@@ -319,7 +319,7 @@ class Walker:
         self.walking_params['knee'] = [msg.data[7],msg.data[8],msg.data[9]]
         self.walking_params['thigh'] = [msg.data[10],msg.data[11],msg.data[12]]
         self.walking_params['hip'] = [msg.data[13],msg.data[14],msg.data[15]]
-        
+
         self.wb_walkerfunc=WholeBodyWalkerFunction(self.walking_params)
 
         self.initial_wq = self.wb_walkerfunc.get(True, 0, [0,0,0])  #First joint configuration to start the walking motion
@@ -328,26 +328,26 @@ class Walker:
         j_names=self.initial_wq.keys()
         for jn in j_names:
             print(jn + str(":") + str(self.initial_wq[jn]))
-            
+
     def _cb_cmd_restart(self,msg):
         """
         Processes cmd_restart and to start a new trial
         """
         print("Walker restart command received")
-        
+
         #Stop the running thread
         while self.displacing or self.walking or self._th_walk:
             rospy.loginfo('Stopping walking thread')
             self.stop()
-            
+
         #If the robot is simuated -> send to initial configuration
         if not self.real_robot:
             rospy.loginfo("Sending robot to zero configuration")
             for jn in self.robotis_mini_ci.q_names:
                 self.pubs[jn].publish(0.0)
-        
+
             time.sleep(1)
-        
+
             #If the robot is simulated -> reset simulation
             try:
                self.pause_simulation_srv()
@@ -361,7 +361,7 @@ class Walker:
                time.sleep(1)
             except rospy.ServiceException as e:
                print("Service call failed: %s"%e)
-        
+
     def _cb_cmd_vel(self,msg):
         """
         Processes cmd_vel and update walker speed
@@ -372,14 +372,14 @@ class Walker:
         vt=msg.angular.z
         self.start()
         self.set_desired_velocity(vx,vy,vt)
-    
+
     def _cb_cmd_stop(self,msg):
         """
         Processes cmd_stop
         """
         print("Walker stop command received: ")
         self.stop()
-        
+
     def goto_initial_wq(self):
         """
         If not there yet, go to initial walking configuration
@@ -390,27 +390,27 @@ class Walker:
             print("Initial configuration")
             print(self.initial_wq)
             self.robotis_mini_ci.set_qd_interpolated(self.initial_wq, 2)
-            rospy.sleep(2)   
+            rospy.sleep(2)
         rospy.loginfo("Initial walking configuration reached")
         print("Distance",self.get_qdist_to_initial_wq() )
-        
+
     def start(self):
         if not self.displacing:
-            self.displacing=True                    
+            self.displacing=True
             self.goto_initial_wq()
             self._th_walk=Thread(target=self._do_walk)
             self._th_walk.start()
             self.walking=True
-    
+
     def stop(self):
         if self.displacing:
             self.walking=False
             rospy.loginfo("Waiting for stopped")
             while not rospy.is_shutdown() and self._th_walk is not None:
-                rospy.sleep(0.1)                
+                rospy.sleep(0.1)
             rospy.loginfo("Stopped")
             self.displacing=False
-            
+
     def set_desired_velocity(self,x,y,t):
         self.desired_velocity=[x,y,t]
 
@@ -420,7 +420,7 @@ class Walker:
         """
         rospy.loginfo("Started walking thread")
         wb_walkerfunc=self.wb_walkerfunc
-        
+
         # Global walk loop
         n=50
         print("Thread rate", 1.0/(self._cycle_period/(2.0*n)))
@@ -435,17 +435,38 @@ class Walker:
             #    self.update_current_velocity(self.desired_velocity, n)
             #    r.sleep()
             #    continue
-            x=float(i)/n            
+            x=float(i)/n
             qd_curr=wb_walkerfunc.get(p, x, self.current_velocity)
             self.update_current_velocity(self.desired_velocity, n)
-            self.robotis_mini_ci.set_qd(qd_curr)
+            # self.robotis_mini_ci.set_qd(qd_curr)
+            """SHAOYU: Config for inital lunge is below. Couldn't figure out how to only make it for inital pose without messing with walking dynamics.
+            If you can modify the dynamics to do the lunge step then this position can be moved to the init"""
+            q = self.robotis_mini_ci.get_q()
+            # print(q) = {'l_ankle_joint': -0.180333122616811, 'l_biceps_joint': 0.0065833793878766755, 'l_elbow_joint': 0.0019420239226439762, 'l_foot_joint': -0.008631522656934898, 'l_hip_joint': -0.0031592425562481097, 'l_knee_joint': 0.7181374554697539, 'l_shoulder_joint': 0.00027758580840409053, 'l_thigh_joint': -0.7016925014464581, 'r_ankle_joint': -0.19299919751523653, 'r_biceps_joint': 0.0065869914035960875, 'r_elbow_joint': 0.0019115820328847732, 'r_foot_joint': -0.007614713592440481, 'r_hip_joint': -0.001811670298975443, 'r_knee_joint': 0.7175071049052848, 'r_shoulder_joint': 0.00031156670689291843, 'r_thigh_joint': -0.6899415977544354}
+            q['r_hip_joint'] = 0
+            q['r_thigh_joint'] = -math.pi/2
+            q['r_knee_joint'] = math.pi/2
+            q['r_ankle_joint'] = 0
+            q['r_foot_joint'] = 0
+            q['l_hip_joint'] = 0
+            q['l_thigh_joint'] = 0
+            q['l_knee_joint'] = math.pi/2
+            q['l_ankle_joint'] = -math.pi/2
+            q['l_foot_joint'] = 0
+            q['r_shoulder_joint'] = math.pi/2
+            q['r_biceps_joint'] = math.pi/2
+            q['r_elbow_joint'] = 0
+            q['l_shoulder_joint'] = 0
+            q['l_biceps_joint'] = 0
+            q['l_elbow_joint'] = math.pi/2
+            self.robotis_mini_ci.set_qd(q)
             i+=1
             if i>n:
                 i=0
                 p=not p
             r.sleep()
         rospy.loginfo("Finished walking thread")
-        
+
         self._th_walk=None
 
     def is_displacing(self):
@@ -456,7 +477,7 @@ class Walker:
         for v in self.current_velocity:
             if abs(v)>e: return True
         return False
-                    
+
     def update_current_velocity(self, target_velocity, n):
         """
         A pseudo-interpolation to a target velocity
@@ -464,7 +485,7 @@ class Walker:
         a=3/float(n)
         b=1-a
         self.current_velocity=[a*tv+b*cv for (tv,cv) in zip(target_velocity, self.current_velocity)]
-        
+
     def get_qdist_to_initial_wq(self):
         """
         Computes the absolute distance between the current robot joint state and the initial walking configuration
@@ -488,17 +509,17 @@ def get_distance(qa_dict, qb_dict):
 
 if __name__=="__main__":
     rospy.init_node("walker")
-    
+
     parser = argparse.ArgumentParser(description='Walker trajectory generator')
     parser.add_argument('--real',action='store_true', help='define when using the real robot')
-    
+
     options, args = parser.parse_known_args()
-    
+
     if options.real:
         rospy.loginfo("Real Robot!")
     else:
         rospy.loginfo("Simulated Robot!")
-    
+
     try:
         rospy.loginfo("Instantiating RobotisMini RobotisMiniControlInterface")
         robotis_mini_ci=RobotisMiniControlInterface(real_robot=options.real)
@@ -506,7 +527,7 @@ if __name__=="__main__":
         walker=Walker(robotis_mini_ci, options.real)
     except rospy.exceptions.ROSInterruptException:
         sys.exit(1)
- 
+
     rospy.loginfo("RobotisMini Walker Ready")
     while not rospy.is_shutdown():
         #for jn in walker.robotis_mini_ci.q_names:
